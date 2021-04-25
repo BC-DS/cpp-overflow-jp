ab0d79dcb98dac57b3e8e7b9d8319c39547ab1b6
Jan Pearce <jan_pearce@berea.edu>
2021-04-25 15:13:50 +0000

    fix main

---
+++ .berea/build/CMakeFiles/l03-enhance_fraction.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/l03-enhance_fraction.dir/home/runner/cpp-overflow-jp/main.cpp.o"
+  "l03-enhance_fraction.pdb"
+  "l03-enhance_fraction"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/l03-enhance_fraction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
